22a2c8f8e6e75b8ea6582479637019703adac239
tako <kidyelman@gmail.com>
2016-10-30 19:54:46 -0400

    added authMethod interface, and pinAuth class implementing said interface - need DB to finalize

---
+++ BioPass/authMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioPass {
+    interface authMethod {
+        int IdentifyUser(object A);
+        Boolean VerifyUser(object A, int user_id);
+    }
+}
---
+++ BioPass/pinAuth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioPass {
+    class pinAuth : authMethod {
+        public int IdentifyUser(object A) {
+            String pin = (String)A;
+            // This will then look user up in the DB by pin
+            return 0;
+        }
+
+        public bool VerifyUser(object A, int user_id) {
+            String pin = (String)A;
+            // Look user up by pin in the DB and compare returned UID to user_id provided - need DB
+            return false;
+        }
+    }
+}
