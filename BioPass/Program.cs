260c25ade0d295560811c47679dbfca28f653634
TheoHauser <theo.hauser@temple.edu>
2016-10-31 15:02:46 -0400

    added db handler (not currently encrypting data)

--- BioPass/Program.cs
+++ BioPass/Program.cs
@@ -29,7 +29,7 @@ namespace BioPass {
 
         public static void recieveCapture(Bitmap finger, Bitmap face, String pin) {
             face.Save(tempFacePath);
-            finger.Save(tempFingerPath);
+          //  finger.Save(tempFingerPath);
         }
     }
 }
