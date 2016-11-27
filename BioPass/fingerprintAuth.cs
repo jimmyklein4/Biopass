0cd5eec7a15f808e46a50e98b1a442ee85e8c722
tako <kidyelman@gmail.com>
2016-11-27 14:43:46 -0500

    re-adding fingerprintAuth file

---
+++ BioPass/fingerprintAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioPass {
+    class fingerprintAuth : authMethod {
+        public int IdentifyUser(object A) {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyUser(object A, int user_id) {
+            throw new NotImplementedException();
+        }
+    }
+}
