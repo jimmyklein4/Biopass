009c91de832761f5d484d71fb0943302e4c2256f
Sam <code@temple.edu>
2016-11-30 23:01:17 -0500

    fingerprintview ui

---
+++ BioPass/fingerprintView.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BioPass {
+    partial class fingerprintView {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.fingerprintList = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fingerprint = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.fingerprintList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fingerprintList
+            // 
+            this.fingerprintList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fingerprintList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fingerprintList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Fingerprint,
+            this.Delete});
+            this.fingerprintList.Location = new System.Drawing.Point(12, 12);
+            this.fingerprintList.Name = "fingerprintList";
+            this.fingerprintList.Size = new System.Drawing.Size(349, 279);
+            this.fingerprintList.TabIndex = 0;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            // 
+            // Fingerprint
+            // 
+            this.Fingerprint.HeaderText = "Fingerprint";
+            this.Fingerprint.Name = "Fingerprint";
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            // 
+            // fingerprintView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 302);
+            this.Controls.Add(this.fingerprintList);
+            this.Name = "fingerprintView";
+            this.Text = "fingerprintView";
+            ((System.ComponentModel.ISupportInitialize)(this.fingerprintList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView fingerprintList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewImageColumn Fingerprint;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
+    }
+}
---
+++ BioPass/fingerprintView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BioPass {
+    public partial class fingerprintView : Form {
+        public fingerprintView() {
+            InitializeComponent();
+        }
+    }
+}
