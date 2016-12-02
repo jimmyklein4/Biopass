91e48fa225d1e575fdf7e70e698cd21f84814525
Robert Woods <robertwoods2013@gmail.com>
2016-12-02 11:29:34 -0500

    updated to fix IOOB error

--- BioPass/IrisAuth.cs
+++ BioPass/IrisAuth.cs
@@ -113,7 +113,7 @@ namespace BioPass
                     minDistance,
                     minSize,
                     maxSize);
-                if (circles.Length > 0)
+                if (circles[0].Length > 0)
                     return circles[0][0];
                 currAcumulator--;
             }
